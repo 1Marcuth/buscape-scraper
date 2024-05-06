18e1b4d2e47498967f9e75fa8f0d259382e5c24b
Marcuth <thiago.playgames15@gmail.com>
2024-05-06 15:01:18 -0300

    Commit Inicial

--- buscape/buscape.py
+++ buscape/buscape.py
@@ -2,6 +2,9 @@ class Buscape:
     def __init__(self) -> None:
         pass
 
-    def get_best_offers(shop_id: str) -> list[dict]: ...
+    def get_best_offers(shop_id: str) -> list[dict]:
+        url = f"https://www.buscape.com.br/cupom-de-desconto/{shop_id}/melhores-ofertas"
 
-    def search(query: str) -> list[dict]: ...
+
+    def search(query: str) -> list[dict]:
+        url = f"https://buscape.com.br/search?q={query}"
