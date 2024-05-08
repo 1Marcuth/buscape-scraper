039aced6a9a1636fb389b4ad055c822af0f212fc
Marcuth <thiago.playgames15@gmail.com>
2024-05-08 16:37:44 -0300

    Aprimorando os seletores e pegando a imagem certa na hora de buscar as melhores ofertas de uma loja

--- buscape/buscape.py
+++ buscape/buscape.py
@@ -23,14 +23,14 @@ class Buscape:
         response = httpx.get(url)
         selector = Selector(response.text)
 
-        product_selectors = selector.css(".ProductCard_ProductCard_Inner__mdriI")
+        product_selectors = selector.css("[class^='ProductCard_ProductCard_Inner']")
         products = []
 
         for product_selector in product_selectors:
             product = {
-                "title": product_selector.css(".ProductCard_ProductCard_Name__7UxBX::text").get(),
-                "image_url": product_selector.css(".ProductCard_ProductCard_Image__lG_w9 img[loading='lazy']::attr(src), .ProductCard_ProductCard_Image__lG_w9 noscript img::attr(src)").get(),
-                "price": Buscape._string_price_to_number(product_selector.css(".Text_MobileHeadingS__XS_Au::text").get()),
+                "title": product_selector.css("[class^='ProductCard_ProductCard_Name'] h2::text").get(),
+                "image_url": product_selector.css("[class^='ProductCard_ProductCard_Image'] img[loading='lazy']::attr(src)").get(),
+                "price": Buscape._string_price_to_number(product_selector.css("[data-testid='product-card::price']::text").get()),
                 "page_url": product_selector.css("::attr(href)").get()
             }
 
@@ -43,15 +43,15 @@ class Buscape:
         response = httpx.get(url)
         selector = Selector(response.text)
 
-        product_selectors = selector.css(".ProductCard_ProductCard_Inner__gapsh")
+        product_selectors = selector.css("[class^='ProductCard_ProductCard_Inner']")
         products = []
 
         for product_selector in product_selectors:
             product = {
-                "title": product_selector.css(".ProductCard_ProductCard_Name__U_mUQ::text").get(),
-                "image_url": product_selector.css(".ProductCard_ProductCard_Image__4v1sa img::attr(src)").get(),
-                "price": Buscape._string_price_to_number(product_selector.css(".Text_MobileHeadingS__HEz7L::text").get()),
-                "page_url": product_selector.css("::attr(href)").get()
+                "title": product_selector.css("[class^='ProductCard_ProductCard_Name'] h2::text").get(),
+                "image_url": product_selector.css("[class^='ProductCard_ProductCard_Image'] img::attr(src)").get(),
+                "price": Buscape._string_price_to_number(product_selector.css("[data-testid='product-card::price']::text").get()),
+                "page_url": "https://www.buscape.com.br" + product_selector.css("::attr(href)").get()
             }
 
             products.append(product)
